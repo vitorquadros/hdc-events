5193a538cb7639842f3c6c8754d7fb4b1533838d
Vitor Quadros <viitorquadros@hotmail.com>
2021-09-01 14:04:46 -0300

    feat: create Event model

--- routes/web.php
+++ routes/web.php
@@ -7,12 +7,3 @@ Route::get('/', [EventController::class, 'index']);
 
 Route::get('/events/create', [EventController::class, 'create']);
 
-Route::get('/products', function () {
-    $search = request('search');
-    return view('products', ['search' => $search]);
-});
-
-Route::get('/products/{id}', function ($id) {
-    return view('product', ['id' => $id]);
-});
-
