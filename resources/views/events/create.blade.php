169bc86d348d9b6cd6e6e4b1688048aa35e2f1f9
Vitor Quadros <viitorquadros@hotmail.com>
2021-09-02 11:31:44 -0300

    feat (create events html): finish form

--- resources/views/events/create.blade.php
+++ resources/views/events/create.blade.php
@@ -4,6 +4,30 @@
 
 @section('content')
 
-<p>Criar evento</p>
+<div id="event-create-container" class="col-md-6 offset-md-3">
+  <h1>Crie o seu evento</h1>
+  <form action="/events" method="POST">
+    <div class="form-group">
+      <label for="title">Evento:</label>
+      <input type="text" class="form-control" id="title" name="title" placeholder="Nome do evento">
+    </div>
+    <div class="form-group">
+      <label for="city">Cidade:</label>
+      <input type="text" class="form-control" id="city" name="city" placeholder="Local do evento">
+    </div>
+    <div class="form-group">
+      <label for="private">O evento é privado?</label>
+      <select name="private" id="private" class="form-control">
+        <option value="0">Não</option>
+        <option value="1">Sim</option>
+      </select>
+    </div>
+    <div class="form-group">
+      <label for="title">Descrição:</label>
+      <textarea name="description" id="description" class="form-control" placeholder="O que vai acontecer no evento?"></textarea>
+    </div>
+    <input type="submit" value="Criar evento" class="btn btn-primary">
+  </form>
+</div>
 
 @endsection
