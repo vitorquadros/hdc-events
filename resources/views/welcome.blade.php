3dd4e772f595a5176fd4bb162e7f21b2a831bc8e
Vitor Quadros <viitorquadros@hotmail.com>
2021-09-12 20:54:50 -0300

    feat (home html): add date

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -19,7 +19,7 @@
       <div class="card col-md-3">
         <img src="/img/events/{{ $event->image }}" alt="{{ $event->title }}">
         <div class="card-body">
-          <p class="card-date">10/09/2021</p>
+          <p class="card-date">{{ date('d/m/Y', strtotime($event->date)) }}</p>
           <h5 class="card-title">{{ $event->title }}</h5>
           <p class="card-participants">X Participantes</p>
           <a href="/events/{{ $event->id }}" class="btn btn-primary">Saber mais</a>
