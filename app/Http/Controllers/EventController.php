1ae74a875f9c7cf2345554e124fa94e26182a718
Vitor Quadros <viitorquadros@hotmail.com>
2021-09-12 21:03:43 -0300

    feat (Event Controller -> index): add search

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -9,9 +9,19 @@ use App\Models\Event;
 class EventController extends Controller
 {
   public function index() {
-    $events = Event::all();
+    $search = request('search');
 
-    return view('welcome', ['events' => $events]);
+    if ($search) {
+      $events = Event::where([
+        ['title', 'like', '%'.$search.'%']
+      ])->get();
+    } else {
+      $events = Event::all();
+    }
+
+    
+
+    return view('welcome', ['events' => $events, 'search' => $search]);
   }
 
   public function create() {
