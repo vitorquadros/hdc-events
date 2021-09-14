0d3ed3d1cf163037f71ccf90fc8a76844b184790
Vitor Quadros <viitorquadros@hotmail.com>
2021-09-14 18:49:12 -0300

    feat (route): add leave event route

--- routes/web.php
+++ routes/web.php
@@ -12,3 +12,4 @@ Route::delete('/events/{id}', [EventController::class, 'destroy'])->middleware('
 Route::get('/events/edit/{id}', [EventController::class, 'edit'])->middleware('auth');
 Route::put('/events/update/{id}', [EventController::class, 'update'])->middleware('auth');
 Route::post('/events/join/{id}', [EventController::class, 'joinEvent'])->middleware('auth');
+Route::delete('/events/leave/{id}', [EventController::class, 'leaveEvent'])->middleware('auth');
